d901bf00ed47ccd3dc58db17d8ff95dcd744659c
Hasnain-ansari <92669424+Hasnain-ansari@users.noreply.github.com>
2023-08-21 01:06:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function roll(){
         let dieone = Math.floor(Math.random()* 6);
         let dietwo = Math.floor(Math.random()* 6);
         console.log(dieone, dietwo);
-        document.querySelector("#die-1").setAttribute("src", "/images/" + images[dieone]);
-        document.querySelector("#die-2").setAttribute("src", "/images/" + images[dietwo]);
+        document.querySelector("#die-1").setAttribute("src", "images/" + images[dieone]);
+        document.querySelector("#die-2").setAttribute("src", "images/" + images[dietwo]);
         document.querySelector("#total").innerHTML = "your roller is " + ((dieone+1) + (dietwo+1));
     }, 1000);
 }
